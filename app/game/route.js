f01e80d7908332b144dd3d3560c92f762794e0ae
Eric Lifka <eric.lifka@gmail.com>
2015-06-05 23:21:32 -0400

    dummy data

--- app/game/route.js
+++ app/game/route.js
@@ -3,7 +3,14 @@ import Ember from 'ember';
 const GameRoute = Ember.Route.extend({
     model(params) {
         return {
-            id: params.game_id
+            id: params.game_id,
+            board: {
+                rows: [
+                    [{type: 'normal'},{type: 'normal'},{type: 'normal'}],
+                    [{type: 'normal'},{type: 'normal'},{type: 'normal'}],
+                    [{type: 'normal'},{type: 'normal'},{type: 'normal'}]
+                ]
+            }
         };
     }
 });
