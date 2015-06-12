a414f224b070ad8392b26a1d886492c27da9c5b9
Eric Lifka <eric.lifka@gmail.com>
2015-06-12 07:10:15 -0400

    use models instead of pojos

--- app/services/store.js
+++ app/services/store.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
+import Board from '../models/board';
+import Game from '../models/game';
+
 export default Ember.Service.extend({
     newGame(id, size = 10) {
-        const game = Ember.Object.create({id});
-        const rows = [];
+        const board = Board.create();
+        const game = Game.create({id, board});
 
+        const rows = [];
         for (let height = size % 2 === 0 ? size + 1 : size,
                  i = 0; i < height; i++) {
 
@@ -20,8 +24,7 @@ export default Ember.Service.extend({
 
             rows.push(row);
         }
-
-        game.set('board', Ember.Object.create({rows}));
+        board.set('rows', rows);
 
         return game;
     }
