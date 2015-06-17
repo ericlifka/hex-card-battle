3fa3c912b9c4f1194cef5f59f5d94c9089c0d091
Eric Lifka <eric.lifka@gmail.com>
2015-06-17 01:06:19 -0400

    track active hex so that old active can be cleared

--- app/models/game.js
+++ app/models/game.js
@@ -4,12 +4,34 @@ export default Ember.Object.extend({
     id: null,
     board: null,
 
+    activeHex: null,
+
     clickHex(hex) {
-        hex.toggleProperty('active');
+        const active = this.get('activeHex');
+        if (active !== hex) {
+            this.deactivateHex(active);
+            this.activateHex(hex);
+            this.set('activeHex', hex);
+        } else {
+            this.deactivateHex(hex);
+            this.set('activeHex', null);
+        }
+    },
+
+    activateHex(hex) {
+        if (hex) {
+            hex.set('active', true);
+        }
+    },
+
+    deactivateHex(hex) {
+        if (hex) {
+            hex.set('active', false);
+        }
     },
 
     lookupHex(x, y, z) {
-        const col = x + (z + (z&1)) / 2;
+        const col = x + (z + (z & 1)) / 2;
         const row = z;
         const board = this.get('board');
 
