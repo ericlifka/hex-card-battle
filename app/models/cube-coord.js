dd3b58a8f58db4d891391bd697fe305e04c90136
Eric Lifka <eric.lifka@gmail.com>
2015-06-19 23:10:51 -0400

    add static builder for cube coords that can validate the zero constraint

--- app/models/cube-coord.js
+++ app/models/cube-coord.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 const CubeCoord = Ember.Object.extend({
     x: 0,
     y: 0,
-    z: 0
+    z: 0,
+
+    adjacentCoords() {
+
+    }
 });
 
 CubeCoord.reopenClass({
@@ -12,6 +16,11 @@ CubeCoord.reopenClass({
         const z = row;
         const y = -x - z;
 
+        return CubeCoord.create({x, y, z});
+    },
+
+    fromCube(x, y, z) {
+        Ember.assert("x + y + z must equal 0", x + y + z === 0);
         return CubeCoord.create({x, y, z});
     }
 });
