d0e8dbb5edd682acf1823568381cd26b29c15296
Eric Lifka <eric.lifka@gmail.com>
2015-06-26 01:10:30 -0400

    replacing select view with custom select as that's the new world order

--- app/components/setup-game/component.js
+++ app/components/setup-game/component.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     classNames: ['setup-game-component'],
-    names: [2, 3, 4]
+
+    playerCounts: [2, 3, 4],
+
+    actions: {
+        change() {
+            console.log('change');
+        }
+    }
 });
