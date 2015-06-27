a94834e99a23c36ce26cbab7c4ad0da0fd76b9eb
Eric Lifka <eric.lifka@gmail.com>
2015-06-27 09:46:08 -0400

    add an option for board size, clearly going to need a component because this is stupid

--- app/components/setup-game/component.js
+++ app/components/setup-game/component.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
     playerCounts: [2, 3, 4],
     selectedPlayerCount: 2,
 
+    boardSizes: ['small', 'medium', 'large'],
+    selectedBoardSize: 'small',
+
     actions: {
         change() {
             const selectedEl = this.$('select#playerCount')[0];
@@ -14,6 +17,15 @@ export default Ember.Component.extend({
             const selectedCount = playerCounts[selectedIndex];
 
             this.set('selectedPlayerCount', selectedCount);
+        },
+
+        changeBoardSize() {
+            const selectedEl = this.$('select#boardSize')[0];
+            const selectedIndex = selectedEl.selectedIndex;
+            const playerCounts = this.get('boardSizes');
+            const selectedSize = playerCounts[selectedIndex];
+
+            this.set('selectedBoardSize', selectedSize);
         }
     }
 });
