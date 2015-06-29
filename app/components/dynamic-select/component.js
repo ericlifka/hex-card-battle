a2f59d43d958eda272dcff3e9e40cf51eaabc898
Eric Lifka <eric.lifka@gmail.com>
2015-06-28 20:15:13 -0400

    add an 'id' and 'for' to link label and select elements

--- app/components/dynamic-select/component.js
+++ app/components/dynamic-select/component.js
@@ -4,5 +4,9 @@ export default Ember.Component.extend({
     classNames: ['dynamic-select'],
 
     description: null,
-    contents: null
+    contents: null,
+
+    uniqueId: Ember.computed(function () {
+        return Ember.generateGuid(this, "dynamic-select-");
+    })
 });
