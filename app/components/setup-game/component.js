5fa6623d8100c9613fb40d4fa2a015e90eedbfb4
Eric Lifka <eric.lifka@gmail.com>
2015-06-28 20:45:32 -0400

    convert old selects over to new helper

--- app/components/setup-game/component.js
+++ app/components/setup-game/component.js
@@ -3,29 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['setup-game-component'],
 
-    playerCounts: [2, 3, 4],
-    selectedPlayerCount: 2,
+    playerCountOptions: [2, 3, 4],
+    playerCount: 2,
 
-    boardSizes: ['small', 'medium', 'large'],
-    selectedBoardSize: 'small',
-
-    actions: {
-        change() {
-            const selectedEl = this.$('select#playerCount')[0];
-            const selectedIndex = selectedEl.selectedIndex;
-            const playerCounts = this.get('playerCounts');
-            const selectedCount = playerCounts[selectedIndex];
-
-            this.set('selectedPlayerCount', selectedCount);
-        },
-
-        changeBoardSize() {
-            const selectedEl = this.$('select#boardSize')[0];
-            const selectedIndex = selectedEl.selectedIndex;
-            const playerCounts = this.get('boardSizes');
-            const selectedSize = playerCounts[selectedIndex];
-
-            this.set('selectedBoardSize', selectedSize);
-        }
-    }
+    boardSizeOptions: ['small', 'medium', 'large'],
+    boardSize: 'small'
 });
