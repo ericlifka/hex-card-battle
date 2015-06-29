a821b8ce31a658726cd247c801b4c66fe9f74b31
Eric Lifka <eric.lifka@gmail.com>
2015-06-28 20:25:23 -0400

    add the dynamic behavior

--- app/components/dynamic-select/component.js
+++ app/components/dynamic-select/component.js
@@ -5,8 +5,23 @@ export default Ember.Component.extend({
 
     description: null,
     contents: null,
+    selectedValue: null,
 
     uniqueId: Ember.computed(function () {
         return Ember.generateGuid(this, "dynamic-select-");
-    })
+    }),
+
+    actions: {
+        change() {
+            const contents = this.get('contents');
+            const action = this.get('action');
+
+            const selectedOption = this.$("select option:selected");
+            const index = selectedOption.index();
+            const selected = contents[index];
+
+            this.set('selectedValue', selected);
+            action(selected);
+        }
+    }
 });
