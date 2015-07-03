894ae74b2d22b826fb095d1c1c54a1477c0fee6f
Eric Lifka <eric.lifka@gmail.com>
2015-07-03 17:12:43 -0400

    base player descriptions off number of players selected

--- app/components/setup-game/component.js
+++ app/components/setup-game/component.js
@@ -3,18 +3,41 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['setup-game-component'],
 
-    playerCountOptions: [2, 3, 4],
-    playerCount: 2,
-
-    boardSizeOptions: ['small', 'medium', 'large'],
     boardSize: 'small',
+    boardSizeOptions: ['small', 'medium', 'large'],
 
-    boardShapeOptions: ['hexagon', 'square', 'random'],
     boardShape: 'square',
+    boardShapeOptions: ['hexagon', 'square', 'random'],
+
+    playerCount: 2,
+    playerCountOptions: [2, 3, 4],
+
+    players: Ember.computed('playerCount', function () {
+        const playerCount = this.get('playerCount');
+        const players = [];
+
+        for (let playerNumber = 0; playerNumber < playerCount; playerNumber++) {
+            players.push(this.playerDescription(playerNumber));
+        }
+
+        return players;
+    }),
 
     actions: {
         createGame() {
             this.sendAction('action', this.getProperties('playerCount', 'boardSize', 'boardShape'));
         }
+    },
+
+    playerDescription(playerNumber) {
+        if (!this._playerCache) {
+            this._playerCache = {};
+        }
+
+        if (!this._playerCache.hasOwnProperty(playerNumber)) {
+            this._playerCache[playerNumber] = Ember.Object.create({playerNumber, name: ""});
+        }
+
+        return this._playerCache[playerNumber];
     }
 });
