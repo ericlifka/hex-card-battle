a8cc8ad97f1a60993cd21b2dc9fc232d42a6871e
Eric Lifka <eric.lifka@gmail.com>
2015-07-02 23:10:52 -0400

    jazz up guid utility a bit

--- app/utils/guid.js
+++ app/utils/guid.js
@@ -1,10 +1,13 @@
-function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
+const s4 = () =>
+    Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
         .substring(1);
-}
 
-export default function guid() {
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
+const s8 = () => s4() + s4();
+const s12 = () => s4() + s8();
+
+const guid = () => [
+        s8(), s4(), s4(), s12()
+    ].join('-');
+
+export default guid;
