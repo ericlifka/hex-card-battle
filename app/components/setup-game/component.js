848bed66aa648570fab9abf9552cd59b0932182e
Eric Lifka <eric.lifka@gmail.com>
2015-07-03 18:15:36 -0400

    send players along to the store to create the game

--- app/components/setup-game/component.js
+++ app/components/setup-game/component.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend({
 
     actions: {
         createGame() {
-            this.sendAction('action', this.getProperties('playerCount', 'boardSize', 'boardShape'));
+            this.sendAction('action', this.getProperties('playerCount', 'boardSize', 'boardShape', 'players'));
         }
     },
 
