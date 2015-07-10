fe86928ceb7645ceddb96293ecdd5e547ea5b6c8
Eric Lifka <eric.lifka@gmail.com>
2015-07-09 22:42:02 -0400

    pass in player data to the component

--- app/components/player-display/component.js
+++ app/components/player-display/component.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['player-display']
+    classNames: ['player-display'],
+
+    game: null,
+    players: Ember.computed.alias('game.players')
 });
