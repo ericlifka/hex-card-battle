5f9b2c328a44326ce7ded829f264bd681eda9f05
Eric Lifka <eric.lifka@gmail.com>
2015-07-10 21:55:33 -0400

    bring in ionicons

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,8 @@
 /* global require, module */
 
+var funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
@@ -10,4 +13,16 @@ var app = new EmberApp({
     }
 });
 
-module.exports = app.toTree();
+app.import('bower_components/ionicons/css/ionicons.css');
+
+var ionicons = funnel('bower_components/ionicons/fonts', {
+    srcDir: '/',
+    destDir: '/fonts'
+});
+
+module.exports = mergeTrees([
+    app.toTree(),
+    ionicons
+], {
+    overwrite: true
+});
