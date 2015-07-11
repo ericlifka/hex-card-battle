2389f316952283fa5371ae397a0a3b87b3faf050
Eric Lifka <eric.lifka@gmail.com>
2015-07-11 01:02:21 -0400

    make the zoom css work

--- app/components/game-board/component.js
+++ app/components/game-board/component.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
+const ZOOM_MIN = 0;
+const ZOOM_MAX = 6;
+
 export default Ember.Component.extend({
     classNames: ['game-board'],
 
+    zoom: 2,
+
     actions: {
         zoomIn() {
-            console.log('zoom-in');
+            this.set('zoom', Math.max(this.get('zoom') - 1, ZOOM_MIN));
         },
         zoomOut() {
-            console.log('zoom-out');
+            this.set('zoom', Math.min(this.get('zoom') + 1, ZOOM_MAX));
         }
     }
 });
--- app/components/hex-board/component.js
+++ app/components/hex-board/component.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['hex-board'],
-    game: null
+    classNameBindings: [':hex-board', 'zoomClass'],
+
+    game: null,
+    zoom: null,
+
+    zoomClass: Ember.computed('zoom', function () {
+        return `zoom-${this.get('zoom')}`;
+    })
 });
