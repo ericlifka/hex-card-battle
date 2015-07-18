350aa3327c9898f530fa74f2390c3e37aa7b64f8
Eric Lifka <eric.lifka@gmail.com>
2015-07-18 13:59:42 -0400

    more randomization helpers

--- app/utils/rand.js
+++ app/utils/rand.js
@@ -1,3 +1,7 @@
 export var range = (max, min) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export var bool = (weight = 0.5) => {
+    return Math.random() < weight;
+};
