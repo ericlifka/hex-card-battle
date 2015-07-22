ff87f9f4e3ecbca2250d0936ad9ee984f841c6df
Eric Lifka <eric.lifka@gmail.com>
2015-07-21 23:19:50 -0400

    zoom-control makes more sense rendered within the game board instead of the whole game

--- app/components/game-board/component.js
+++ app/components/game-board/component.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
-const ZOOM_MIN = 0;
-const ZOOM_MAX = 4;
-
 export default Ember.Component.extend({
-    classNames: ['game-board'],
-
-    zoom: 1,
-
-    actions: {
-        zoomIn() {
-            this.set('zoom', Math.max(this.get('zoom') - 1, ZOOM_MIN));
-        },
-        zoomOut() {
-            this.set('zoom', Math.min(this.get('zoom') + 1, ZOOM_MAX));
-        }
-    }
+    classNames: ['game-board']
 });
--- app/components/hex-board/component.js
+++ app/components/hex-board/component.js
@@ -1,12 +1,25 @@
 import Ember from 'ember';
 
+const ZOOM_MIN = 0;
+const ZOOM_MAX = 4;
+
 export default Ember.Component.extend({
     classNameBindings: [':hex-board', 'zoomClass'],
 
     game: null,
-    zoom: null,
+    zoom: 1,
 
     zoomClass: Ember.computed('zoom', function () {
         return `zoom-${this.get('zoom')}`;
-    })
+    }),
+
+    actions: {
+        zoomIn() {
+            this.set('zoom', Math.max(this.get('zoom') - 1, ZOOM_MIN));
+        },
+
+        zoomOut() {
+            this.set('zoom', Math.min(this.get('zoom') + 1, ZOOM_MAX));
+        }
+    }
 });
