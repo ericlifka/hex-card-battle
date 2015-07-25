17a97550f7cecd6501ccd73ee86c3cd004956952
Eric Lifka <eric.lifka@gmail.com>
2015-07-25 13:52:17 -0400

    action for changing the state

--- app/components/card-market/component.js
+++ app/components/card-market/component.js
@@ -3,9 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNameBindings: [':card-market', 'expandedClass'],
 
+    expanded: false,
+
     expandedClass: Ember.computed('expanded', function () {
         return this.get('expanded') ? 'expanded' : '';
     }),
 
-
+    actions: {
+        togglePanelState() {
+            this.toggleProperty('expanded');
+        }
+    }
 });
