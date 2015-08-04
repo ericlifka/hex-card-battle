c0b833b844c623f28e2e7481acb3b8adcacde141
Eric Lifka <eric.lifka@gmail.com>
2015-08-03 23:45:26 -0400

    create an internal node to show resource instead of coloring the whole tile

--- app/components/hex-board/hex-tile/component.js
+++ app/components/hex-board/hex-tile/component.js
@@ -8,6 +8,12 @@ export default Ember.Component.extend({
     game: null,
     hex: null,
 
+    hasResource: Ember.computed('hex.type', function () {
+        const type = this.get('hex.type');
+
+        return type === 'resource-primary' || type === 'resource-secondary';
+    }),
+
     click() {
         if (this.get('hex.type') !== 'empty') {
             this.get('game').clickHex(this.get('hex'));
