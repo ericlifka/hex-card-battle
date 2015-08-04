97307ba1fcaec9133b9636f2b9a930b8aee2a130
Eric Lifka <eric.lifka@gmail.com>
2015-08-03 22:54:07 -0400

    put some stuff in the starting decks

--- app/utils/deck-generator.js
+++ app/utils/deck-generator.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
+import Card from '../models/card';
+
 export default Ember.Object.create({
     startingDeck() {
-        return [];
+        const name = 'Energy Well';
+        const art = '/art/energy-well.jpg';
+
+        return [
+            Card.create({name, art}),
+            Card.create({name, art}),
+            Card.create({name, art}),
+            Card.create({name, art}),
+            Card.create({name, art})
+        ];
     }
 });
