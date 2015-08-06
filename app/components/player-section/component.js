6229b20aedfd2ca7937986ccf5e866e970d0f1fa
Eric Lifka <eric.lifka@gmail.com>
2015-08-05 20:45:52 -0400

    drop in the expand-contract behavior from the card-market

--- app/components/player-section/component.js
+++ app/components/player-section/component.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['player-section']
+    classNameBindings: [':player-section', 'expandedClass'],
+
+    game: null,
+
+    expanded: false,
+    expandedClass: Ember.computed('expanded', function () {
+        return this.get('expanded') ? 'expanded' : '';
+    }),
+
+    actions: {
+        togglePanelState() {
+            this.toggleProperty('expanded');
+        }
+    }
 });
