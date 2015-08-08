c8caa23d3282f57467d2b9bd5c0af075a8e14987
Eric Lifka <eric.lifka@gmail.com>
2015-08-07 23:42:05 -0400

    switch player-section to wrapper

--- app/components/player-section/component.js
+++ app/components/player-section/component.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNameBindings: [':player-section', 'expandedClass'],
+    classNames: ['player-section'],
 
-    game: null,
-
-    expanded: false,
-    expandedClass: Ember.computed('expanded', function () {
-        return this.get('expanded') ? 'expanded' : '';
-    }),
-
-    actions: {
-        togglePanelState() {
-            this.toggleProperty('expanded');
-        }
-    }
+    game: null
 });
