fa77183573bfe8ed9d4868610e6d5a54db6f8414
Eric Lifka <eric.lifka@gmail.com>
2015-08-07 22:54:00 -0400

    cleanup

--- app/components/card-market/component.js
+++ app/components/card-market/component.js
@@ -1,20 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNameBindings: [':card-market', 'expandedClass'],
+    classNames: ['card-market'],
 
     game: null,
-    cards: Ember.computed.alias('game.cardMarketCards'),
-
-    expanded: false,
-
-    expandedClass: Ember.computed('expanded', function () {
-        return this.get('expanded') ? 'expanded' : '';
-    }),
-
-    actions: {
-        togglePanelState() {
-            this.toggleProperty('expanded');
-        }
-    }
+    cards: Ember.computed.alias('game.cardMarketCards')
 });
