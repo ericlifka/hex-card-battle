e652986559c5641cc14f06c446b4bbebc94ea07e
Eric Lifka <eric.lifka@gmail.com>
2015-08-10 22:20:22 -0400

    adhere to var rule

--- app/utils/rand.js
+++ app/utils/rand.js
@@ -1,7 +1,7 @@
-export var range = (max, min) => {
+export const range = (max, min) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export var bool = (weight = 0.5) => {
+export const bool = (weight = 0.5) => {
     return Math.random() < weight;
 };
