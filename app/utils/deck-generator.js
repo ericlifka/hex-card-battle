02ecd402f0cf59438bf943a25a755698ad739301
Eric Lifka <eric.lifka@gmail.com>
2015-08-28 23:46:14 -0400

    improving some ramda-ness

--- app/utils/deck-generator.js
+++ app/utils/deck-generator.js
@@ -2,13 +2,23 @@ import Ember from 'ember';
 
 import Card from '../models/card';
 
-let Cards;
+const Cards = {
+    EnergyWell: {
+        name: 'Energy Well',
+        art: '/art/energy-well.jpg'
+    },
+    SquirrelScout: {
+        name: 'Squirrel Scout',
+        art: '/art/squirrel-scout.png'
+    }
+};
+const Generators = R.mapObj(def => Card.create(def), Cards);
 
 export default Ember.Object.create({
     startingDeck() {
         return R.flatten([
-            R.map(() => Card.create(Cards.EnergyWell), R.range(0, 8)),
-            R.map(() => Card.create(Cards.SquirrelScout), R.range(0, 2))
+            R.map(Generators.EnergyWell, R.range(0, 8)),
+            R.map(Generators.SquirrelScout, R.range(0, 2))
         ]);
     },
 
@@ -16,16 +26,3 @@ export default Ember.Object.create({
         return [];
     }
 });
-
-Cards = {
-
-    EnergyWell: {
-        name: 'Energy Well',
-        art: '/art/energy-well.jpg'
-    },
-
-    SquirrelScout: {
-        name: 'Squirrel Scout',
-        art: '/art/squirrel-scout.png'
-    }
-};
