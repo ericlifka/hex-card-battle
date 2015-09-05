92ed58512bfc78e3d690aa2b114c358463257dbf
Eric Lifka <eric.lifka@gmail.com>
2015-09-04 22:17:43 -0400

    switch build files in line with ember-cli

---
+++ ember-cli-build.js
@@ -0,0 +1,33 @@
+/* global require, module */
+
+var funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function () {
+
+    var app = new EmberApp({
+        lessOptions: {
+            paths: [
+                'app/components'
+            ]
+        }
+    });
+
+    app.import('bower_components/ionicons/css/ionicons.css');
+    app.import('bower_components/ramda/dist/ramda.js');
+
+    var ionicons = funnel('bower_components/ionicons/fonts', {
+        srcDir: '/',
+        destDir: '/fonts'
+    });
+
+    return module.exports = mergeTrees([
+        app.toTree(),
+        ionicons
+    ], {
+        overwrite: true
+    });
+
+};
