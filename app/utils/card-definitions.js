fdc0efe6b20cf9b38d67a7da4907ff90e8702bf6
Eric Lifka <eric.lifka@gmail.com>
2015-09-05 15:08:45 -0400

    add mechanics definitions to starting cards

--- app/utils/card-definitions.js
+++ app/utils/card-definitions.js
@@ -1,7 +1,41 @@
 export default {
-    EnergyWell: {
-        name: 'Energy Well',
-        art: '/art/energy-well.jpg'
+    ManaGem: {
+        name: 'Mana Gem',
+        art: 'PLACEHOLDER',
+        mechanics: [
+            {
+                type: 'resource-grant',
+                resource: 'mana',
+                count: 1
+            }
+        ]
+    },
+    SimpleOrders: {
+        name: 'SimpleOrders',
+        art: 'PLACEHOLDER',
+        mechanics: [
+            {
+                type: 'resource-grant',
+                resource: 'action',
+                count: 1
+            }
+        ]
+    },
+    MagicalCommand: {
+        name: 'Magical Command',
+        art: 'PLACEHOLDER',
+        mechanics: [
+            {
+                type: 'resource-grant',
+                resource: 'mana',
+                count: 1
+            },
+            {
+                type: 'resource-grant',
+                resource: 'action',
+                count: 1
+            }
+        ]
     },
     SquirrelScout: {
         name: 'Squirrel Scout',
