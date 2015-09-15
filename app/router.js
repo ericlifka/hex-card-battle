11a0fba030815de2fc09557548dfd3a2560f8f25
Eric Lifka <eric.lifka@gmail.com>
2015-09-14 23:28:05 -0400

    overwriting location is bad news bears, who knew?

--- app/router.js
+++ app/router.js
@@ -2,9 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const { Router } = Ember;
-const location = config.locationType;
 
-const AppRouter = Router.extend({ location });
+const AppRouter = Router.extend({ location: config.locationType });
 AppRouter.map(function () {
     this.resource('game', {
         path: '/game/:game_id'
