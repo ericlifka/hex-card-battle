f1cb0021bff5e51397b90d040eb3f3a2c96be055
Eric Lifka <eric.lifka@gmail.com>
2015-09-22 21:21:36 -0400

    render the active player's hand in the player area

--- app/components/player-section/component.js
+++ app/components/player-section/component.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
     classNames: ['player-section'],
 
-    game: null
+    game: null,
+    player: computed.alias('game.phase.activePlayer'),
+    hand: computed.alias('player.hand')
 });
