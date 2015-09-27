80b0c82a0d5d22d41225f153a12815b2d04fdd8a
Eric Lifka <eric.lifka@gmail.com>
2015-09-26 22:09:45 -0400

    smooth out the display text model

--- app/components/interactive-card/component.js
+++ app/components/interactive-card/component.js
@@ -2,9 +2,20 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-    classNames: ['interactive-card'],
+    classNames: [ 'interactive-card' ],
 
     card: null,
+
     name: computed.alias('card.name'),
-    art: computed.alias('card.art')
+    art: computed.alias('card.art'),
+
+    displayText: computed('card.displayText', function () {
+        const text = this.get('card.displayText');
+
+        if (typeof text === "string") {
+            return [ text ];
+        }
+
+        return text;
+    })
 });
