624c0f1fe8516ba5af96bfd783524b1b85ee707d
Eric Lifka <eric.lifka@gmail.com>
2015-09-27 15:35:45 -0400

    move the data smoothing logic to the model because that just makes sense

--- app/components/interactive-card/component.js
+++ app/components/interactive-card/component.js
@@ -8,14 +8,5 @@ export default Component.extend({
 
     name: computed.alias('card.name'),
     art: computed.alias('card.art'),
-
-    displayText: computed('card.displayText', function () {
-        const text = this.get('card.displayText');
-
-        if (typeof text === "string") {
-            return [ text ];
-        }
-
-        return text;
-    })
+    abilities: computed.alias('card.abilities')
 });
--- app/models/card.js
+++ app/models/card.js
@@ -1,6 +1,21 @@
+import Ember from 'ember';
 import GameModel from './model-base';
+
+const { computed } = Ember;
 
 export default GameModel.extend({
     name: '',
-    art: ''
+    art: '',
+    mechanics: '',
+    displayText: '',
+
+    abilities: computed('displayText', function () {
+        const text = this.get('displayText');
+
+        if (typeof text === 'string') {
+            return [ text ];
+        } else {
+            return text;
+        }
+    })
 });
