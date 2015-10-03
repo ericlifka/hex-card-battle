666f2a108eede2f2a2c8cc21155a524a37ee50cf
Eric Lifka <eric.lifka@gmail.com>
2015-10-03 13:16:50 -0400

    laying out ground work for playing cards, this is going to be messy

--- app/components/player-section/component.js
+++ app/components/player-section/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 const { Component, computed } = Ember;
 
 export default Component.extend({
-    classNames: ['player-section'],
+    classNames: [ 'player-section' ],
 
     game: null,
     player: computed.alias('game.phase.activePlayer'),
@@ -10,7 +10,36 @@ export default Component.extend({
 
     actions: {
         playCard(card) {
+            let ability = card.mechanics[ 0 ];
+            if (card.mechanics.length > 1) {
+                // player needs to choose which ability to use
+            }
 
+            switch (ability.type) {
+                case 'resource-grant':
+                    this.resourceGrant(ability);
+                    break;
+
+                case 'summon-unit':
+                    break;
+
+                default:
+                    console.error(`Unrecognized mechanic type: ${ability.type}`);
+            }
         }
+    },
+
+    resourceGrant(ability) {
+        let resourceProperty;
+        if (ability.resource === 'action') {
+            resourceProperty = 'actions';
+        } else if (ability.resource === 'mana') {
+            resourceProperty = 'mana';
+        } else {
+            console.error(`Unrecognized resource type: ${ability.resource}`);
+            return;
+        }
+
+        this.get('player.resources').incrementProperty(resourceProperty);
     }
 });
