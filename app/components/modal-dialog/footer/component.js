80c6bb466190ebf450e20df2a5348aba34b5dd68
Eric Lifka <eric.lifka@gmail.com>
2015-10-10 15:53:07 -0400

    add acknowledge button to the footer

--- app/components/modal-dialog/footer/component.js
+++ app/components/modal-dialog/footer/component.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import CancelModalMixin from '../mixins/cancel-modal';
+import AcknowledgeModalMixin from '../mixins/acknowledge-modal';
 const { Component } = Ember;
 
-export default Component.extend(CancelModalMixin, {
+export default Component.extend(CancelModalMixin, AcknowledgeModalMixin, {
     classNames: ['modal-footer']
 });
