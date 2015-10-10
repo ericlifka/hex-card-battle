531fb93b57aece953bd74d348c6c5b7661c707d0
Eric Lifka <eric.lifka@gmail.com>
2015-10-10 15:33:19 -0400

    clean up imports

--- app/components/modal-dialog/footer/component.js
+++ app/components/modal-dialog/footer/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import CancelModalMixin from '../mixins/cancel-modal';
-const { Component, computed } = Ember;
+const { Component } = Ember;
 
 export default Component.extend(CancelModalMixin, {
     classNames: ['modal-footer']
