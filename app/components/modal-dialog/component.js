76db4f457019961289e063bef238376b4144128d
Eric Lifka <eric.lifka@gmail.com>
2015-10-24 23:07:42 -0400

    remove the old modal

--- app/components/modal-dialog/component.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-const { Component, computed } = Ember;
-
-export default Component.extend({
-    classNames: ['modal-dialog']
-});
--- app/components/modal-dialog/header/component.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import CancelModalMixin from '../mixins/cancel-modal';
-const { Component } = Ember;
-
-export default Component.extend(CancelModalMixin, {
-    classNames: ['modal-header'],
-    label: ""
-});
