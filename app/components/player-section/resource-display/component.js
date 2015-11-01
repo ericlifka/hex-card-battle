b59f9a4c29862e0db0203009d1eac2a95065ac26
Eric Lifka <eric.lifka@gmail.com>
2015-10-31 23:00:52 -0400

    add references to the player's resources so the resource section can do it's jerb

--- app/components/player-section/resource-display/component.js
+++ app/components/player-section/resource-display/component.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 const {
-    Component
+    Component,
+    computed
 } = Ember;
 
 export default Component.extend({
-    classNames: [ 'resource-display' ]
+    classNames: [ 'resource-display' ],
+
+    player: computed.alias('game.phase.activePlayer'),
+
+    actions: computed.alias('player.resources.actions'),
+    mana: computed.alias('player.resources.mana')
 });
